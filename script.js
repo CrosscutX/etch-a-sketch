2dae21a2e692a0145a814ed132622ae891ff840d
Alex <crosscutcode@gmail.com>
2022-05-24 19:32:31 -0400

    Add hover functionality

--- script.js
+++ script.js
@@ -1,17 +1,29 @@
 const boxContainer = document.querySelector('.box-container');
 
+let boxRow = document.createElement('div'); 
+let box = document.createElement('div'); 
 
 function createGrid(){
     
     for(let column = 0; column < 16; column++){
-        let boxRow = document.createElement('div');
+        boxRow = document.createElement('div');
         boxRow.className = 'box-row';
         boxContainer.appendChild(boxRow);
+        
         for(let row = 0; row < 16; row++){
-            let box = document.createElement('div');
+            box = document.createElement('div');
             box.className = 'box';
             boxRow.appendChild(box);
         }
     }
 }
 createGrid();
+
+const sketch = document.querySelectorAll('.box');
+console.log(sketch);
+
+sketch.forEach(box => {
+    box.addEventListener('mouseenter' , () => {
+        box.style.backgroundColor = 'black';
+    });
+});
