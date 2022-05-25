b223e2062785811e1d6dff4fc0735f84733c24ba
Alex <crosscutcode@gmail.com>
2022-05-25 15:23:59 -0400

    Reset button largely functional

--- script.js
+++ script.js
@@ -2,15 +2,15 @@ const boxContainer = document.querySelector('.box-container');
 
 let boxRow = document.createElement('div'); 
 let box = document.createElement('div'); 
-
-function createGrid(){
+//loops through to make initial grid
+function createGrid(size = 16){
     
-    for(let column = 0; column < 16; column++){
+    for(let column = 0; column < size; column++){
         boxRow = document.createElement('div');
         boxRow.className = 'box-row';
         boxContainer.appendChild(boxRow);
         
-        for(let row = 0; row < 16; row++){
+        for(let row = 0; row < size; row++){
             box = document.createElement('div');
             box.className = 'box';
             boxRow.appendChild(box);
@@ -19,11 +19,43 @@ function createGrid(){
 }
 createGrid();
 
-const sketch = document.querySelectorAll('.box');
-console.log(sketch);
 
+const btn = document.querySelector('BUTTON');
+let sketch = document.querySelectorAll('.box');
+let rowClass = document.querySelectorAll('.box-row')
+let resize = 0;
+
+//Allows the grid to color black on mouse hover
 sketch.forEach(box => {
     box.addEventListener('mouseenter' , () => {
         box.style.backgroundColor = 'black';
     });
 });
+
+//Click event for reset button
+
+btn.addEventListener('click', () =>{
+    resize = prompt('Enter a number between 10 and 100!');
+    if(resize >= 10 && resize <= 100){
+        rowClass.forEach(element => {
+            element.parentNode.removeChild(element)
+        });
+        createGrid(resize);
+    
+        //This is important, without reassiging rowClass and sketch we will have nothing 
+        //selected and reset will not work the second time
+        
+        rowClass = document.querySelectorAll('.box-row');
+        sketch = document.querySelectorAll('.box');
+        sketch.forEach(box => {
+            box.addEventListener('mouseenter' , () => {
+                box.style.backgroundColor = 'black';
+            });
+        });
+    }else{
+        alert('Invalid input');
+    }
+});
+
+
+
